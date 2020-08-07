f8c7aaf56c4e4ee6b98993d7d3184f0b3c19adfa
bill245364863 <xiaotao245364863@qq.com>
2020-08-07 08:29:30 +0800

    内容分类

--- taotaocontent/taotao-content-interface/src/main/java/com/content/service/ContentCategoryService.java
+++ taotaocontent/taotao-content-interface/src/main/java/com/content/service/ContentCategoryService.java
@@ -1,9 +1,17 @@
 package com.content.service;
 
 import com.taotao.common.EasyUITreeNod;
+import com.taotao.common.TaotaoResult;
 
 import java.util.List;
 
 public interface ContentCategoryService {
+
     List<EasyUITreeNod> getContentCategoryList(long parentId);
+
+    TaotaoResult addContentCategory(Long parentId,String categoryName);
+
+    void updateContentCategory(Long id,String name);
+
+    void deleteContentCategory(Long id);
 }
